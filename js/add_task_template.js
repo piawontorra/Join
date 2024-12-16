4f9f29341a924fa2f8d8fd987b96632495013cf4
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2024-12-16 22:48:16 +0100

    added the bottom of the Add Task Section

--- js/add_task_template.js
+++ js/add_task_template.js
@@ -1,16 +1,3 @@
-function getAssignedToTemplate(i) {
-  return `
-        <label for="checkbox${i}">
-            <li class="contact-list" id="contactList${i}">        
-                <div tabindex="0" class="emblem" style="background-color: ${contact.color}">
-                  ${contact.emblem}
-                </div> 
-                <div class="contact-name" >${usersAddTask}</div> 
-                <input class="user-checkbox" onclick="showUsersEmblem()" type="checkbox" id="checkbox${i}" data-userid="${contact.userId}">          
-            </li>
-        </label> `;
-}
-
 function getSubtaskTemplate(i) {
     return `
       <div class="subtask-list" id="mainSubtask-container${i}">
