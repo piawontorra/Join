d213c46b198c8b0845ded5035d7b694b92d768ad
kikiAlbrecher <christina.albrecher@gmx.at>
2024-12-16 01:37:21 +0100

    changes because of deleted template.js

---
+++ js/registerTemplate.js
@@ -0,0 +1,7 @@
+function getRegistrySuccessTemplate() {
+    return `
+        <div class="registration-success">
+            <div class="btn btn1 btn-success">You Signed Up successfully.</div>
+        </div>
+    `;
+}
