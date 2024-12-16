4495306343ac3b6e9fd25ebb5c1a9f8adfed390b
BedirhanMehmetSoylu <soylu.bedirhanmehmet@icloud.com>
2024-12-16 19:28:52 +0100

    changed the style of the assignedTo container

--- js/add_task_template.js
+++ js/add_task_template.js
@@ -1,3 +1,16 @@
+function getAssignedToTemplate(i) {
+  return `
+        <label for="checkbox${i}">
+            <li class="contact-list" id="contactList${i}">        
+                <div tabindex="0" class="emblem" style="background-color: ${contact.color}">
+                  ${contact.emblem}
+                </div> 
+                <div class="contact-name" >${usersAddTask}</div> 
+                <input class="user-checkbox" onclick="showUsersEmblem()" type="checkbox" id="checkbox${i}" data-userid="${contact.userId}">          
+            </li>
+        </label> `;
+}
+
 function getSubtaskTemplate(i) {
     return `
       <div class="subtask-list" id="mainSubtask-container${i}">
