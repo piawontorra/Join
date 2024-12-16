81a9c6fb2fdbdc490e33272d2f258c908f4b86f3
kikiAlbrecher <christina.albrecher@gmx.at>
2024-12-16 01:39:02 +0100

    changes because of deleted template.js

--- js/board_template.js
+++ js/board_template.js
@@ -1,11 +1,3 @@
-function getRegistrySuccessTemplate() {
-    return `
-        <div class="registration-success">
-            <div class="btn btn1">You Signed Up successfully.</div>
-        </div>
-    `;
-}
-
 function getTaskCardTemplate(task) {
     let icon = priorityIcons[task.priority];
 
