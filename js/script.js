a564effe81b30c56ef24030627ece7d2952e6221
kikiAlbrecher <christina.albrecher@gmx.at>
2024-12-20 10:29:51 +0100

    added login password visibility

--- js/script.js
+++ js/script.js
@@ -4,18 +4,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const msg = urlParams.get('msg');
 const msgBox = document.getElementById('msg-box');
 
-// logo-animation sollte nur beim ersten Mal in der session abgespielt Werden. Funktioniert noch nicht.
-window.onload = function () {
-    if (!sessionStorage.getItem('logoAnimated')) {
-        document.getElementById('logo-animation').classList.add('animate-logo');
-        sessionStorage.setItem('logoAnimated', 'true');
-    }
-};
-
-if (msg) {
-    msgBox.innerHTML = msg;
-}
-
 async function initLogin() {
     includeHTML();
     let userResponse = await loadUsers("users");
@@ -33,18 +21,54 @@ async function loadUsers(path = "") {
     return responseAsJson = await response.json();
 }
 
+window.onload = function () {
+    if (!sessionStorage.getItem('logoAnimated')) {
+        document.getElementById('logo-animation').classList.add('animate-logo');
+        sessionStorage.setItem('logoAnimated', 'true');
+    }
+};
+
+if (msg) {
+    msgBox.innerHTML = msg;
+}
+
 function openRegistry() {
     window.location.href = 'register.html';
 }
 
+function changePasswordImg() {
+    let passwordRef = document.getElementById('password').value;
+    let passwordImgRef = document.getElementById('password-img');
+
+    passwordRef.length > 0 ? passwordImgRef.src = "./assets/img/invisible.png" : passwordImgRef.src = "./assets/img/lock-icon.png";
+}
+
+function togglePasswordVisibility() {
+    let passwordField = document.getElementById("password");
+    let passwordImgRef = document.getElementById("password-img");
+
+    switch (passwordField.type) {
+        case "password":
+            passwordField.type = "text";
+            passwordImgRef.src = "./assets/img/visible.png";
+            break;
+        default:
+            passwordField.type = "password";
+            passwordImgRef.src = "./assets/img/invisible.png";
+            break;
+    }
+}
+
 function login() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let user = users.find(user => user.user.email === email && user.user.password === password);
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(user => user.user.email === email.value && user.user.password === password.value);
 
     if (user) {
+        let userName = user.user.name;
+        console.log(userName);
+
         transferToSummary();
-        document.getElementById('login-form').reset();
         resetFields();
     }
     else {
@@ -61,7 +85,9 @@ function adaptFields() {
 
 function resetFields() {
     document.getElementById('input-email').classList.remove('red-border');
+    email.value = '';
     document.getElementById('input-password').classList.remove('red-border');
+    password.value = '';
     document.getElementById('msg-box').innerHTML = '';
 }
 
