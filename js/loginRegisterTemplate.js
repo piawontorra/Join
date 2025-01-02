313a41164e91a87312872143f31a9e40f438b79a
kikiAlbrecher <christina.albrecher@gmx.at>
2025-01-02 19:12:36 +0100

    added documentation

--- js/loginRegisterTemplate.js
+++ js/loginRegisterTemplate.js
@@ -1,3 +1,10 @@
+/**
+ * Generates an HTML template for a success message displayed in an overlay.
+ * This template includes a button with a given success message.
+ *
+ * @param {string} message - The success message to display on the button.
+ * @returns {string} The HTML string for the success message overlay.
+ */
 function getSuccessTemplate(message) {
     return `
         <div class="overlay-message">
@@ -6,6 +13,12 @@ function getSuccessTemplate(message) {
     `;
 }
 
+/**
+ * Generates an HTML template for a login error message.
+ * This template contains a simple error message instructing the user to check their credentials.
+ *
+ * @returns {string} The HTML string for the login error message.
+ */
 function getLoginErrorTemplate() {
     return `
         <p>Check your email and password. Please try again.</p>
